7fd5f74d13e82634e95fcb2205ec0095beea75d2
Bradley <bmcguigandev@outlook.com>
2020-05-22 21:39:53 +0100

    add save feature

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module Dictionary
     def dictionary
         File.read("../dictionary.txt")
@@ -39,16 +41,19 @@ class Player
         @guesses = guesses
     end
 
-    def get_guess
+    def get_input
         puts "\n"
-        puts "Please guess a letter:"
-        guess = gets.chomp.downcase
-        guess = ensure_valid_guess(guess)
-        guess = ensure_new_guess(guess)
-        
+        puts "Please guess a letter or enter \'save\' to save your game:"
+        input = gets.chomp.downcase
 
-        @guesses += guess
-        guess
+        return input if input.downcase == "save"
+    
+        input = ensure_valid_guess(input)
+        input = ensure_new_guess(input)
+        
+        
+        @guesses += input
+        return input
     end
 
 
@@ -87,20 +92,64 @@ class Player
 end
 
 class Game
-    def initialize(player, computer, guesses_left=15)
+    attr_reader :player, :computer, :guesses_left
+
+    def initialize(player=Player.new, computer=Computer.new, guesses_left=15)
         @player = player
         @computer = computer
         @guesses_left = guesses_left
     end
 
+    def self.start
+        puts "Would you like to load a saved game?(Y/N)"
+        input = gets.chomp
+
+        while !input.match(/[ynYN]/)
+            puts "\n"
+            puts "Please enter \'Y\' for yes or \'N\' for no:"
+            input = gets.chomp
+            puts "\n"
+        end
+
+        if input.upcase == "Y"
+            Game.load.play
+        else
+            Game.new.play
+        end
+    end
+
+    def self.load
+        puts "Please enter the name of the save file you wish to load:"
+        file_name = gets.chomp
+        file = File.read("../savegames/#{file_name}")
+        saved_game = Game.unserialize(file)
+    end
+
+    def serialize
+        YAML.dump(self)
+    end
+
+    def self.unserialize(string)
+        YAML.load(string)
+    end
+
     def play
+
         computer_chose_word_message
         puts "\n"
+
         while true
             get_incorrect_guesses
-            guess = @player.get_guess
+
+            input = @player.get_input
+            if input == 'save'
+                save_game
+                saved_game_message
+                next
+            end
+
             puts "\n"
-            matching_letters_message(@computer.word, guess)
+            matching_letters_message(@computer.word, input)
             puts @computer.secret_word(@player.guesses)
             puts "\n"
             remaining_guesses
@@ -122,7 +171,15 @@ class Game
         end
     end
 
+
    private
+
+    def save_game
+        Dir.mkdir("../savegames") if !Dir.exists?("../savegames")
+
+        time = Time.now.to_s[0..18]
+        File.write("../savegames/#{time}", self.serialize)
+    end
 
     def matching_letters_message(word, guess)
         if word.include?(guess)
@@ -152,6 +209,14 @@ class Game
         puts "Your previous incorrect guesses are:"
         puts "#{incorrect_guesses.join(", ")}."
     end
+
+    def saved_game_message
+        puts "\n"
+        puts "-"*25
+        puts "Game successfully saved."
+        puts "-"*25
+        puts "\n"
+    end
     
     def victory_message
         puts "\n"
@@ -166,7 +231,7 @@ class Game
     end
 
     def game_over?
-        out_of_guesses? || guessed_word? ? true : false
+        guessed_word? || out_of_guesses?
     end
 
     def out_of_guesses?
@@ -174,7 +239,11 @@ class Game
     end
 
     def guessed_word?
-        @player.guesses.split("").sort == @computer.word.split("").sort
+        @computer.word.split("").each do |letter|
+            return false if !@player.guesses.include?(letter)
+        end
+
+        true
     end
 
     def computer_chose_word_message
@@ -187,8 +256,4 @@ class Game
     
 end
 
-
-player = Player.new
-computer = Computer.new
-game = Game.new(player, computer)
-game.play
+Game.start
